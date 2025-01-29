9fcf7b12765ab8e9b3dec0087f12e9a5d155a469
aceDavon <makojidavid@gmail.com>
2025-01-29 11:32:28 +0100

    Improve Authentication Logic, attach authRoutes, Implement property validation helper function

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import AuthService from "../app/features/auth"
+import { validateArrayProperties } from "src/utils/handlers/propertyValidation"
 
 export const register = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -14,9 +15,13 @@ export const register = async (req: Request, res: Response): Promise<void> => {
 export const login = async (req: Request, res: Response): Promise<void> => {
   try {
     const { username, password } = req.body
+    
+    if (!validateArrayProperties([username, password])) throw new Error("Missing credentials")
+    
     const token = await AuthService.login(username, password)
     res.status(200).json({ token })
   } catch (error) {
-    res.status(401).json({ error })
+    const { message } = error as { message: string }
+    res.status(400).json({ error: message })
   }
 }
